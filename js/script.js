ddb0a5a495e32148ba97f2d7e78e17445010c99d
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-04-17 00:18:56 +0000

    final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// script.js
+/* Function to convert kilos to pounds */
+function convertKilosToPounds() {
+    // Get input value
+    var kilos = parseFloat(document.getElementById("inputKilos").value);
+
+    // Check if input is a valid number
+    if (!isNaN(kilos)) {
+        // Perform conversion
+        var pounds = kilos * 2.20462;
+        // Round to 2 decimal places
+        pounds = Math.round(pounds * 100) / 100;
+        // Display result
+        document.getElementById("result").innerHTML = "Result: " + kilos + " kilos is approximately " + pounds + " pounds.";
+    } else {
+        // Display error message if input is not a number
+        document.getElementById("result").innerHTML = "Please enter a valid number.";
+    }
+}
